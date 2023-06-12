559bc36f2ffba7cadcb4af2d7e76c43903579275
VadimUa <vadia62beck@gmail.com>
2023-06-12 21:08:29 +0300

    my cursova

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+"use strict"
+function scroll(elem){
+    window.scrollTo({
+        top: elem.offsetTop,
+        behavior: "smooth"
+    });
+}
+let secondPart = document.querySelector('.white_line');
+let mapnure = document.querySelector('.map__nure');
+let overlay = document.querySelector(".map__nure_overlay");
+overlay.addEventListener("click", () =>{
+    overlay.classList.add("active");
+})
+
+let downArrow = document.querySelector('.buttondown');
+downArrow.addEventListener('click',() => {
+    scroll(secondPart);
+})
+
+let know = document.querySelector('.jsbutton');
+know.addEventListener('click',() => {
+    window.scrollTo({
+        top:mapnure.offsetTop,
+        behavior:'smooth'
+    });
+})
+let center_line = document.getElementById('center_line');
+center_line.addEventListener('click',()=>{
+window.location.href = 'https://www.youtube.com/@nuretv';
+})
